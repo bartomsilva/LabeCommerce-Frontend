06cbef02be284d37d195489e27fcf9569dcb9e67
bartomsilva <bartomsilva@gmail.com>
2023-03-10 09:29:36 -0300

    fix: css

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import ProductsProvider from './components/Context/ProductsContext'
 import App from './App'
 import LoadUpdateData from './components/Data/LoadUpdateData'
-import Reset from './Styles/Reset'
-import Base from './Styles/base'
+import Reset from './styles/Reset'
+import Base from './styles/base'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>

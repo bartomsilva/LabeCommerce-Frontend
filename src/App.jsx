c52005b94391b91ac8a970915e15bdb2109c7f6d
bartomsilva <bartomsilva@gmail.com>
2023-03-10 09:45:53 -0300

    fix: bugs ao tentar fazer o deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header"
 import Aside from "./components/Aside/Aside"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
-import ShoppingCart, { updateStatusCart } from "./components/Shoppingcart/ShoppingCart"
+import ShoppingCart, { updateStatusCart } from "./components/ShoppingCart/ShoppingCart"
 import { Container } from "./Styles/styled"
 
 export default function App() {

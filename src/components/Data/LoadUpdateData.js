6d2a60ddc4f09da3e2edbff69277a0b2a3e27e80
bartomsilva <bartomsilva@gmail.com>
2023-03-22 07:57:43 -0300

    fix

--- src/components/Data/LoadUpdateData.js
+++ src/components/Data/LoadUpdateData.js
@@ -5,45 +5,45 @@ import { saveCart } from "../Libs/MyLib"
 import { updateStatusCart } from "../ShoppingCart/ShoppingCart"
 
 export default function LoadData() {
-    const {
-        cart,
-        setCart,
-        setProducts,
-        setItemsCart,
-        setTotalCart,
-        setTotalProducts
-    } = useContext(ProductsContext)
+  // const response = await axios.get("https://project-react-pilares.bartomsilva.repl.co")
+  const {
+    cart,
+    setCart,
+    setProducts,
+    setItemsCart,
+    setTotalCart,
+    setTotalProducts
+  } = useContext(ProductsContext)
 
-    useEffect(() => {
-        (async () => {
-            try {
-                // const response = await axios.get("https://project-react-pilares.bartomsilva.repl.co")
-                const response = await axios.get("/static/Data.json")
-                setProducts(response.data);
-                setTotalProducts(response.data.length)
-            }
-            catch (error) {
-                console.log(error);
-            };
-        })()
-    }, [])
+  // ler dados dos produtos
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/static/Data.json")
+        setProducts(response.data);
+        setTotalProducts(response.data.length)
+      }
+      catch (error) {
+        console.log(error);
+      };
+    })()
+  }, [])
 
-    // criar ou ler o carrinho
+  // criar ou ler o carrinho
+  useEffect(() => {
+    if (!localStorage.shoppingCart) {
+      saveCart(cart)
+    } else {
+      setCart(JSON.parse(localStorage.getItem("shoppingCart")))
+    }
+  }, [])
 
-    useEffect(() => {
-        if (!localStorage.shoppingCart) {
-          saveCart(cart)
-        } else {
-          setCart(JSON.parse(localStorage.getItem("shoppingCart")))
-        }
-      }, [])
-        
-      useEffect(() => {
-        updateStatusCart(cart, setItemsCart, setTotalCart)
-      }, [cart])
+  useEffect(() => {
+    updateStatusCart(cart, setItemsCart, setTotalCart)
+  }, [cart])
 
 
-    
+
 
 }
 
